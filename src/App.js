78855de1313d84247497a309707215a4e2213822
Andrey Grachov <andrey.grachov@gmail.com>
2023-07-20 22:02:56 +0300

    initial page

---
+++ src/App.js
@@ -0,0 +1,69 @@
+import './App.css';
+
+function App() {
+  return (
+    <div style={{width: 1280, height: 800, position: 'relative'}}>
+  <div style={{width: 1280, height: 800, left: 0, top: 0, position: 'absolute', background: '#593B94'}}></div>
+  <div style={{width: 885, height: 404, left: 398, top: -4, position: 'absolute'}}>
+    <img style={{width: 484, height: 203, left: 0, top: 201, position: 'absolute'}} src="/logo.png" />
+    <div style={{width: 314, height: 62, left: 571, top: 0, position: 'absolute'}}>
+      <div style={{width: 122, height: 57, left: 0, top: 0, position: 'absolute'}}>
+        <div style={{left: 0, top: 0, position: 'absolute'}}><span style={{color: 'white', fontSize: 47, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>Login</span><span style={{color: 'white', fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>         </span></div>
+        <div style={{width: 115, height: 4, left: 0, top: 50, position: 'absolute', background: 'white'}}></div>
+      </div>
+      <div style={{width: 8, height: 8, left: 147, top: 32, position: 'absolute', background: 'white'}}></div>
+      <div style={{width: 131, height: 57, left: 183, top: 5, position: 'absolute'}}>
+        <div style={{left: 0, top: 0, position: 'absolute'}}><span style={{color: 'white', fontSize: 47, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>Editor</span><span style={{color: 'white', fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>         </span></div>
+        <div style={{width: 115, height: 4, left: 0, top: 50, position: 'absolute', background: 'white'}}></div>
+      </div>
+    </div>
+  </div>
+  <div style={{width: 880, height: 248, left: 400, top: 552, position: 'absolute'}}>
+    <div style={{width: 489, height: 206, left: 0, top: 0, position: 'absolute'}}>
+      <div style={{width: 147, height: 205, left: 0, top: 1, position: 'absolute'}}>
+        <div style={{width: 147, height: 157, left: 0, top: 0, position: 'absolute'}}>
+          <div style={{width: 147, height: 157, left: 0, top: 0, position: 'absolute', background: '#B478FF'}}></div>
+          <div style={{width: 106.64, height: 130, left: 21, top: 21, position: 'absolute'}}>
+            <img style={{width: 106, height: 101, left: 0, top: 0, position: 'absolute'}} src="https://via.placeholder.com/106x101" />
+            <div style={{left: 1.64, top: 103, position: 'absolute'}}><span style={{color: 'white', fontSize: 22, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>AAAAAAA</span><span style={{color: 'white', fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>             </span></div>
+          </div>
+        </div>
+        <div style={{width: 134.37, height: 39, left: 0.63, top: 166, position: 'absolute'}}>
+          <div style={{left: 0, top: 0, position: 'absolute'}}><span style={{color: 'white', fontSize: 31, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>by </span><span style={{color: 'white', fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>           </span></div>
+          <div style={{left: 41.37, top: 1, position: 'absolute'}}><span style={{color: 'white', fontSize: 31, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>@user</span><span style={{color: 'white', fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>           </span></div>
+        </div>
+      </div>
+      <div style={{width: 147, height: 205, left: 171, top: 1, position: 'absolute'}}>
+        <div style={{width: 147, height: 157, left: 0, top: 0, position: 'absolute'}}>
+          <div style={{width: 147, height: 157, left: 0, top: 0, position: 'absolute', background: '#B478FF'}}></div>
+          <div style={{width: 106.64, height: 130, left: 21, top: 21, position: 'absolute'}}>
+            <img style={{width: 106, height: 101, left: 0, top: 0, position: 'absolute'}} src="https://via.placeholder.com/106x101" />
+            <div style={{left: 1.64, top: 103, position: 'absolute'}}><span style={{color: 'white', fontSize: 22, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>AAAAAAA</span><span style={{color: 'white', fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>             </span></div>
+          </div>
+        </div>
+        <div style={{width: 134.37, height: 39, left: 0.63, top: 166, position: 'absolute'}}>
+          <div style={{left: 0, top: 0, position: 'absolute'}}><span style={{color: 'white', fontSize: 31, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>by </span><span style={{color: 'white', fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>           </span></div>
+          <div style={{left: 41.37, top: 1, position: 'absolute'}}><span style={{color: 'white', fontSize: 31, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>@user</span><span style={{color: 'white', fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>           </span></div>
+        </div>
+      </div>
+      <div style={{width: 147, height: 205, left: 342, top: 0, position: 'absolute'}}>
+        <div style={{width: 147, height: 157, left: 0, top: 0, position: 'absolute'}}>
+          <div style={{width: 147, height: 157, left: 0, top: 0, position: 'absolute', background: '#B478FF'}}></div>
+          <div style={{width: 106.64, height: 130, left: 21, top: 21, position: 'absolute'}}>
+            <img style={{width: 106, height: 101, left: 0, top: 0, position: 'absolute'}} src="https://via.placeholder.com/106x101" />
+            <div style={{left: 1.64, top: 103, position: 'absolute'}}><span style={{color: 'white', fontSize: 22, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>AAAAAAA</span><span style={{color: 'white', fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>             </span></div>
+          </div>
+        </div>
+        <div style={{width: 134.37, height: 39, left: 0.63, top: 166, position: 'absolute'}}>
+          <div style={{left: 0, top: 0, position: 'absolute'}}><span style={{color: 'white', fontSize: 31, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>by </span><span style={{color: 'white', fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>           </span></div>
+          <div style={{left: 41.37, top: 1, position: 'absolute'}}><span style={{color: 'white', fontSize: 31, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>@user</span><span style={{color: 'white', fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>           </span></div>
+        </div>
+      </div>
+    </div>
+    <div style={{width: 1, height: 1, left: 879, top: 247, position: 'absolute', background: '#C8C8C8'}}></div>
+  </div>
+</div>
+  );
+}
+
+export default App;
